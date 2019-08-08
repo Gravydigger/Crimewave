7e34395e3cef09e9a83e6186ab9270d3a2ad5cf9
Rhys Malson <gravydigger65@gmail.com>
2019-08-08 17:58:34 +0930

    Inital Commit. Added new Idle Sprite for Player, and added bow sprite.

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public float playerSpeed = 1;
+    public float speed;
+    //private new Rigidbody2D rigidbody;
+    CameraController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+        //rigidbody.AddForce(playerSpeed * movement);
+        transform.position += movement * playerSpeed * Time.deltaTime;
+
+        speed = Mathf.Sqrt(Mathf.Abs(Input.GetAxisRaw("Horizontal") + Input.GetAxisRaw("Vertical")));
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+
+    [SerializeField] GameObject player;
+    public float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 posOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        Vector2 mouseOnScreen = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        transform.right = mouseOnScreen - posOnScreen;
+
+        angle = Mathf.Rad2Deg * Mathf.Atan2(transform.right.y, transform.right.x);
+    }
+}
