c522d8b7ece37f6a52d7495301152400caa771d5
Rhys Malson <gravydigger65@gmail.com>
2019-08-13 20:18:49 +0930

    Fixed bugs where multiple arrows stay on screen, and arrows move to most recent target.

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -7,22 +7,24 @@ public class Arrow : MonoBehaviour
     WeaponController WC;
 
     private Vector2 direction;
-    private Vector2 target;
+    private Vector2 currentTarget;
     public float arrowVelocity = 1;
     private Vector2 currentPos;
     // Start is called before the first frame update
     void Start()
     {
         WC = WeaponController.instance;
+        currentTarget = WC.target;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, WC.target, arrowVelocity * Time.deltaTime);
+
+        transform.position = Vector2.MoveTowards(transform.position, currentTarget, arrowVelocity * Time.deltaTime);
 
         //See if the arrow has reached its destination
-        if (transform.position == WC.target)
+        if (Vector3.Distance(transform.position, currentTarget) < 0.01f)
         {
             Destroy(gameObject);
         }
