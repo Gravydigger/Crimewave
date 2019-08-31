f6e2b0b8a708bdf373e2758dfed6a87bfdcc5220
Rhys Malson <gravydigger65@gmail.com>
2019-08-31 17:44:13 +0930

    Added bleeding to player depending on health

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class CharacterManager : MonoBehaviour
 {
     public static CharacterManager instance;
+    public ParticleSystem ps;
 
     public float playerSpeed = 6f;
     [HideInInspector] public int maxHealth = 6;
@@ -21,27 +22,7 @@ public class CharacterManager : MonoBehaviour
         instance = this;
         currentHealth = maxHealth;
         SetHealthUI();
-    }
-
-    private void SetHealthUI()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            // make a empty heart by default
-            int index = 2;
-            // make a full heart if health >= 6, 4  or 2 respectively
-            if (currentHealth >= (6-i*2))
-            {
-                index = 0;
-            }
-            // make a half heart if health == 5, 3  or 1 respectively
-            if (currentHealth == (5 - i * 2))
-            {
-                index = 1;
-            }
-            // set the correct sprite
-            hearts[i].sprite = healthSprites[index];
-        }
+        BleedAmount();
     }
 
     private void TakeDamage(int amount)
@@ -53,6 +34,9 @@ public class CharacterManager : MonoBehaviour
 
         //Changes the UI elements appropriately 
         SetHealthUI();
+        
+        //Changes the bleed rate of player
+        BleedAmount();
 
         //If player has 0 or negitive hp, call OnDeath()
         if (currentHealth <= 0 && !isDead)
@@ -70,6 +54,9 @@ public class CharacterManager : MonoBehaviour
 
         //Change the UI elements appropriately
         SetHealthUI();
+
+        //Changes the bleed rate of player
+        BleedAmount();
     }
 
     private void OnDeath()
@@ -78,6 +65,44 @@ public class CharacterManager : MonoBehaviour
         maxHealth = 0;
     }
 
+    private void SetHealthUI()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            // make a empty heart by default
+            int index = 2;
+            // make a full heart if health >= 6, 4  or 2 respectively
+            if (currentHealth >= (6 - i * 2))
+            {
+                index = 0;
+            }
+            // make a half heart if health == 5, 3  or 1 respectively
+            if (currentHealth == (5 - i * 2))
+            {
+                index = 1;
+            }
+            // set the correct sprite
+            hearts[i].sprite = healthSprites[index];
+        }
+    }
+
+    private void BleedAmount()
+    {
+        var bleed = ps.emission;
+        //if player is at 0hp and is not at full health, make them bleed
+        if (currentHealth != 0)
+        {
+            bleed.rateOverTime = Mathf.Abs(currentHealth - (maxHealth + 1)) / 2 * 1.5f;
+        }
+
+        //if player is at 0hp, stop the particle stsyem.
+        else
+        {
+            ps.Stop();
+        }
+
+        Debug.Log("Bleed Rate: " + Mathf.Abs(currentHealth - (maxHealth + 1)) / 2 * 1.5f);
+    }
     /***************************Dev Tools*******************************/
 
     [ContextMenu("Player: Take 1 HP")]
